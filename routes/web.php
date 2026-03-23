37cbc296d63ddf7900793fded0d22dad4a48b529
sanzouh <santaherizo2.0@gmail.com>
2026-03-23 05:20:24 +0300

    refactor: cleanup blade views, web routes and npm artifacts

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,1 @@
 <?php
-
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view("welcome");
-});
-
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
